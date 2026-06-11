d365e8b946b23dc90ecf809b0910b23f8d5f4133
LavleshRamkhalawon <lavleshramkhalawon0@gmail.com>
2026-06-11 21:23:39 +0400

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'moris_agroconnect',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
